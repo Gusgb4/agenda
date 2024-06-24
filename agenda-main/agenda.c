8b212e5c2b5f564445d36022dd54deb7cd7f0257
Gusgb4 <tueloke@gmail.com>
2024-06-23 22:21:21 -0300

    Adicionar Contato e Pesquisar Contato estralando

--- agenda-main/agenda.c
+++ agenda-main/agenda.c
@@ -7,7 +7,7 @@
 typedef struct agenda{ //Typedef serve para definir um nome para simplificar, e o nome é definido no final, nesse está contato
     char nome[30]; 
     char numero[12]; //11 = 47 99999 9999 sem os espaços
-}contato;
+}Contato;
 
 
 
@@ -53,47 +53,87 @@ int main(void){
 }
 
 
-//Funções
+//------- FUNÇÕES --------
 
 void addContato(void){
     
-    contato AddContato; //Define estrutura "Nome" e "Numero", usado la no struct
-    FILE *contatos; //Declarar variavel "contatos"
+    //Variaveis
 
-    contatos = fopen("contatos.txt","a");
-       
-	if (contatos == NULL) {
+    Contato contato; //Define estrutura "Nome" e "Numero", usado la no struct
+    FILE *arquivo; //Declarar variavel "arquivo"
+    arquivo = fopen("contatos.bin","wb"); //Atribui a variavel "Arquivo" ao arquivo | Por que é usado wb? W - write, e o B é por se tratar de binario
+
+
+    //Teste para ver se abre
+
+	if (arquivo == NULL) {
         printf("Erro ao abrir arquivo.\n");
 		return;
     }
 
+
+    //Processo de pedir nome e número
+
     printf("Digite o nome do contato: ");
-    fgets(AddContato.nome, 30, stdin); //variavel, tamanho maximo, tipo da entrada
+    fgets(contato.nome, sizeof(contato.nome), stdin); //variavel, tamanho maximo, tipo da entrada
     fflush(stdin);
 
     printf("Digite o numero do telefone: ");
-    fgets(AddContato.numero, 12, stdin);
+    fgets(contato.numero, sizeof(contato.numero), stdin);
     fflush(stdin);
 
-    fprintf(contatos, "%s%s\n", AddContato.nome, AddContato.numero); //Não precisa do \n para dar um "enter" porque o gets pega o espaço do enter quando a gente escreve
+    fwrite(&contato, sizeof(Contato), 1, arquivo); //Não precisa do \n para dar um "enter" porque o gets pega o espaço do enter quando a gente escreve
 	
-    fclose(contatos);
+    fclose(arquivo);
     printf("\nContato adicionado\n");
     fflush(stdin);
 }
 
 void pesquisarContato(void) {
+        
+    //Variaveis
 
+    Contato contato;
+    FILE *arquivo;
+    char name[31], opcao;
 
+    arquivo = fopen("contatos.bin", "rb");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+
+    printf("---------------------------------------\n");
+    printf("       AGENDA DE CONTATOS - PESQUISA   \n\n");
+    printf("[a] Pesquisa por Nome \n");
+    printf("[b] Pesquisa por Numero \n\n");
+    printf("Digite a sua opcao: ");
+    scanf(" %c",&opcao);
+    fflush(stdin);
+
+    if (opcao == 'a'){
+        printf("Digite o nome: ");
+        fgets(name, 30, stdin);
+
+        while (fread(&contato, sizeof(Contato), 1, arquivo) == 1)
+        {
+            if (strcmp(contato.nome, name) == 0) { //strcmp = string comparate
+                printf("\nNome:%sNumero:%s\n",contato.nome, contato.numero);
+            }
+        }
+    }
 }
+
 
 void excluirContato(void) {
 
-    char NomeExcluir[30];
+    /*char NomeExcluir[30];
     FILE *excluirContato, *Auxiliar;
     contato c;
 
     excluirContato = fopen("contatos.txt", "r");     //Abrir arquivo original para leitura
+
     if (excluirContato == NULL) {
         printf("Erro ao abrir arquivo.\n");
         return;
@@ -101,6 +141,7 @@ void excluirContato(void) {
 
 	
     Auxiliar = fopen("temp.txt", "w");    //Abrir arquivo temporario
+
     if (Auxiliar == NULL) {
         printf("Erro ao abrir arquivo temporario.\n");
         fclose(excluirContato);
@@ -108,11 +149,11 @@ void excluirContato(void) {
     }
 
     printf("Digite o nome do contato a ser excluido: ");
-    scanf(" %29s", &NomeExcluir);
+    scanf("%29s", NomeExcluir);
 
    
     while (fscanf(excluirContato, "%s %s\n", c.nome, c.numero) == 2) {  //Verifica os contatos já salvos e salva no arquivo temporario, exceto o que será excluído.
-        if (strcmp(c.nome, NomeExcluir) != 0) {
+        if (strcmp(c.nome, NomeExcluir) != 0) { //strcmp = string comparate
             fprintf(Auxiliar, "%s\t%s\n", c.nome, c.numero);
         }
     }
@@ -123,5 +164,5 @@ void excluirContato(void) {
 	remove("contatos.txt");  // Remover o arquivo original e renomear o arquivo temporário para o original
     rename("temp.txt", "contatos.txt");
     printf("Contato excluido com sucesso.\n");
-    fflush(stdin);
+    fflush(stdin);*/
 }
